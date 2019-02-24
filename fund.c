05eadc57debcdcdfe6bb37e5fa3728a3ddee975a
JohnSnowball <pandaontheway@hotmail.com>
2019-02-24 20:27:10 +0800

    add hello

--- fund.c
+++ fund.c
@@ -7,31 +7,12 @@
 
 #include "fund.h"
 #include <stdio.h>
-#define FUNDLEN 50
-
-struct funds{
-    char bank[FUNDLEN];
-    double bankfund;
-    char save[FUNDLEN];
-    double savefund
-};
-
-double sum(stuct funds moolah);
-
+#define F 20
 int main(void)
 {
-    struct funds stan={
-        "Garlic-melon Bank",
-        4032.27,
-        "Lucky's Savings and Loan",
-        8573.24
-    };
+    char name[F];
+    scanf("%S",F);
+    printf("%s ,hello!\n", F);
     
-    printf("Stan has a total of $%.2f\n",sum(stan));
     return 0;
-}
-
-double sum(struct funds moolah)
-{
-    return(moolah.bankfund+moolah.savefund);
 }
